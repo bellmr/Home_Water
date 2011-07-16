11d4152650d78b25b69321f7260a971ec0de9a21
Malcolm Bell <bellmr@malmac.local>
2011-07-16 21:04:03 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HomeWater::Application.config.secret_token = '6f27aece94f1f2e3692b84d034def00eadfff11b6a1d61cd9b20e8374103500e2cbb462748a0578e9eff6571742f6e87ca4722087f6708177c528a66e78c7e58'
